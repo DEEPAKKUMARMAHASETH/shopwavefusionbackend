e7941d1c3e6957e38c707ccf433b9f2149302e80
Deepak Mahaseth <deepakkumarmahaseth5@gmail.com>
2023-08-28 15:46:28 +0530

    final coding

--- shopwavefusionbackend/src/main/java/com/shopwavefusion/config/ProjectSecurityConfig.java
+++ shopwavefusionbackend/src/main/java/com/shopwavefusion/config/ProjectSecurityConfig.java
@@ -13,50 +13,49 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-
-import jakarta.servlet.http.HttpServletRequest;
-
 
 @Configuration
 public class ProjectSecurityConfig {
-	
 
+//	 @Override
+//	    public void addCorsMappings(CorsRegistry registry) {
+//	        registry.addMapping("/**")
+//	                .allowedOrigins("*")
+//	                .allowedMethods("GET", "POST", "PUT", "DELETE")
+//	                .allowedHeaders("*");
+//	    }
 
-    @Bean
-    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+	@Bean
+	SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
 
-        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-            .cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
-            @Override
-            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
-                CorsConfiguration config = new CorsConfiguration();
-                config.setAllowedOrigins(Collections.singletonList("http://127.0.0.1:5500"));
-                config.setAllowedMethods(Collections.singletonList("*"));
-                config.setAllowCredentials(true);
-                config.setAllowedHeaders(Collections.singletonList("*"));
-                config.setExposedHeaders(Arrays.asList("Authorization"));
-                config.setMaxAge(3600L);
-                return config;
-            }
-                })).csrf((csrf) -> csrf.disable())
-                .addFilterAfter(new JWTTokenGeneratorFilter(), BasicAuthenticationFilter.class)
-                .addFilterBefore(new JWTTokenValidatorFilter(), BasicAuthenticationFilter.class)
-                .authorizeHttpRequests((requests)->requests
-                		.requestMatchers("/admin/products/**","/admin/orders/**","/admin/control/**").hasRole("ADMIN")
-                		.requestMatchers("/cart/**","/users/**","cart_items/**"
-                				,"/orders/**","/products/**","/ratings/**").hasAnyRole("USER", "ADMIN")
-                		.requestMatchers("/all").hasAnyRole("USER","ADMIN")
-                        .requestMatchers("/auth/signin").authenticated()
-                        .requestMatchers("/notices","/contact","/auth/signup","/swagger-ui*/**","/v3/api-docs/**").permitAll())
-                .formLogin(Customizer.withDefaults())
-                .httpBasic(Customizer.withDefaults());
-        return http.build();
-    }
+		http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+				.cors(corsCustomizer -> corsCustomizer.configurationSource(request -> {
+					CorsConfiguration config = new CorsConfiguration();
+					config.setAllowedOriginPatterns(Collections.singletonList("*"));
+					config.setAllowedMethods(Collections.singletonList("*"));
+					config.setAllowCredentials(true);
+					config.setAllowedHeaders(Collections.singletonList("*"));
+					config.setExposedHeaders(Arrays.asList("Authorization"));
+					config.setMaxAge(3600L);
+					return config;
+				})).csrf((csrf) -> csrf.disable())
+				.addFilterAfter(new JWTTokenGeneratorFilter(), BasicAuthenticationFilter.class)
+				.addFilterBefore(new JWTTokenValidatorFilter(), BasicAuthenticationFilter.class)
+				.authorizeHttpRequests((requests) -> requests
+						.requestMatchers("/admin/products/**", "/admin/orders/**", "/admin/control/**").hasRole("ADMIN")
+						.requestMatchers("/cart/**", "/users/**", "cart_items/**", "/orders/**", "/products/**",
+								"/ratings/**")
+						.hasAnyRole("USER", "ADMIN").requestMatchers("/all").hasAnyRole("USER", "ADMIN")
+						.requestMatchers("/auth/signin").authenticated()
+						.requestMatchers("/notices", "/contact", "/auth/signup", "/swagger-ui*/**", "/v3/api-docs/**")
+						.permitAll())
+				.formLogin(Customizer.withDefaults()).httpBasic(Customizer.withDefaults());
+		return http.build();
+	}
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
 }
